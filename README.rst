f7a894ccc880a2dd201190a5e881decae2d266c5
Fabien Potencier <fabien.potencier@gmail.com>
2014-08-04 22:18:11 +0200

    added initial set of checksums

---
+++ README.rst
@@ -0,0 +1,56 @@
+SensioLabs Checksums (experimental)
+===================================
+
+This repository contains signed SHA1 for all releases made by Open-Source
+projects managed by SensioLabs.
+
+You can use these SHA1 to validate the libraries you installed via a Github ZIP
+file or via Composer.
+
+To check all dependencies installed by Composer, clone this repository and run
+the ``check-vendors.sh`` script:
+
+.. code-block:: bash
+
+    $ PATH/TO/check-vendors.sh
+
+It will output something along the lines of:
+
+.. code-block:: text
+
+    symfony/swiftmailer-bundle@v2.2.6                        OK  files signature
+    symfony/symfony@v2.5.2                                   KO  files signature
+    twig/extensions@v1.0.1                                   OK  files signature
+    twig/twig@v1.15.0                                        OK  files signature
+    white-october/pagerfanta-bundle@dev-master               --  unknown package
+    willdurand/hateoas@1.0.x-dev                             --  unknown package
+
+     1 packages are potentially corrupted.
+     Check that your did not add/modify/delete some files.
+
+You can also check a directory manually by following the following steps. As an
+example, let's say I have a project using Symfony 2.4.2 (you can check the
+version installed by Composer by running ``composer show -i``). The sha1s for
+this specific version are available here:
+https://raw.githubusercontent.com/sensiolabs/checksums/master/symfony/symfony/v2.4.2.txt.
+
+This file is signed, so you first need to verify it:
+
+.. code-block:: bash
+
+    $ curl -O https://raw.githubusercontent.com/sensiolabs/checksums/master/symfony/symfony/v2.4.2.txt
+    $ gpg --verify v2.4.2.txt
+
+.. note::
+
+    The key used to sign the sha1 files is ``0xeb8aa69a566c0795``
+
+Now, you can check the validity of the files you downloaded:
+
+.. code-block:: bash
+
+    $ cd PATH/TO/vendor/symfony/symfony
+    $ find . -type f -print0 | xargs -0 shasum | shasum
+
+The sha1 displayed should match the one from the file you've just downloaded
+(the one under the `files_sha1` entry.)
---
+++ check-vendors.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# Check that signed vendors are ok
+
+CHECKSUMS=$( cd $(dirname $0) ; pwd -P )
+
+# update the checksums
+cd $CHECKSUMS && git pull >/dev/null 2>/dev/null && cd - >/dev/null
+
+BAD=0
+composer show -i > /tmp/packages
+PACKAGES=`cut -d ' ' -f 1 /tmp/packages`
+for PACKAGE in $PACKAGES
+do
+    # Get the installed version
+    VERSION=`grep -E "$PACKAGE " /tmp/packages | tr -s ' ' | cut -d ' ' -f 2`
+
+    printf "%-55s" "$PACKAGE@$VERSION"
+
+    # Do we have a signature for this package?
+    if [ ! -d $CHECKSUMS/$PACKAGE ]; then
+        echo -e " \033[43;37m -- \033[m unknown package"
+        continue
+    fi
+
+    # Do we have a signature for this version?
+    CHECKSUM_FILE=$CHECKSUMS/$PACKAGE/$VERSION.txt
+    if [ ! -f $CHECKSUM_FILE ]; then
+        echo -e " \033[43;37m -- \033[m not signed"
+        continue
+    fi
+
+    # Is it a fixed tag?
+    if [[ $VERSION == dev-* ]]; then
+        echo -e " \033[43;37m -- \033[m no signature"
+        continue
+    fi
+
+    cd vendor/$PACKAGE
+    if [ -d ".git" ]; then
+        TYPE="Git signature"
+        git tag -v $VERSION > /dev/null 2>/dev/null
+        RESULT=$?
+    else
+        TYPE="files signature"
+
+        # Check the file is correct
+        gpg --verify $CHECKSUM_FILE >/dev/null 2>/dev/null
+        if [ $? -ne 0 ]; then
+            RESULT=1
+        else
+            # Extract the sha1 for files
+            SHA1=`grep files_sha1 $CHECKSUM_FILE | tr -s ' ' | cut -d ' ' -f 2`
+
+            # Compute the SHA1 for installed files
+            DIR=$(dirname `find . -name composer.json | head -n1`)
+            CURRENT_SHA1=`cd $DIR && find . -type f -print0 | xargs -0 shasum | shasum |  tr -s ' ' | cut -d ' ' -f 1`
+
+            # Check that the sha1 in the signed file is the same
+            [ "$SHA1" == "$CURRENT_SHA1" ]
+            RESULT=$?
+        fi
+    fi
+
+    if [ $RESULT -eq 0 ]; then
+        echo -e " \033[42;37m OK \033[m $TYPE"
+    else
+        echo -e " \033[41;37m KO \033[m $TYPE"
+        BAD=$(($BAD + 1))
+    fi
+
+    cd ../../..
+done
+
+echo ""
+if [ $BAD -ne 0 ]; then
+    echo -e "\033[41;37m $BAD packages are potentially corrupted. \033[m"
+    echo -e "\033[41;37m Check that your did not add/modify/delete some files. \033[m"
+    exit 1
+else
+    echo -e "\033[42;37m Great! Checked packages are trusted. \033[m"
+fi
